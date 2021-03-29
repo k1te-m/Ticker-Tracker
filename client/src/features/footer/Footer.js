7c6e8f62ebb4820fcdbb5945b7276ea27d4ec0e7
k1te <kmiller343@gmail.com>
2021-03-29 12:25:52 -0500

    basic footer component

---
+++ client/src/features/footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container-fluid">
+        <div className="row">
+          <a href="https://iexcloud.io">Data provided by IEX Cloud</a>
+        </div>
+        <div className="row">
+          <span>Ticker Tracker © 2021</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
