05749e8733e701c70c029cc176922338d93f8ec8
k1te <kmiller343@gmail.com>
2021-03-29 13:34:02 -0500

    Mongoose user model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: "Please enter a valid password.",
+    trim: true,
+  },
+  register_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
