d95e8f507cb7efebab16b25de790a52ee83c4f15
k1te <kmiller343@gmail.com>
2021-03-29 12:26:53 -0500

    placeholder jumbotron component

---
+++ client/src/features/landing/jumbotron/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Jumbotron = () => {
+  return (
+    <div className="container-fluid jumbotron">
+      <div className="row">
+        <h3 className="welcome">Welcome to Ticker Tracker!</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
