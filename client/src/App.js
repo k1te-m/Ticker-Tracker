6b5f8097bb576e6a79c7b543d25cd92545ff092b
k1te <kmiller343@gmail.com>
2021-03-29 14:11:33 -0500

    created privateroute for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./features/routing/PrivateRoute";
 import Alert from "./features/alert/Alert";
 import Landing from "./features/landing/Landing";
 import Login from "./features/login/Login";
 import Register from "./features/register/Register";
 import "./sass/main.scss";
+import Dashboard from "./features/dashboard/Dashboard";
 
 function App() {
   return (
@@ -15,6 +17,9 @@ function App() {
           <Route exact path="/" component={Landing} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <PrivateRoute exact path="/dashboard">
+            <Dashboard />
+          </PrivateRoute>
         </Switch>
       </Router>
     </div>
