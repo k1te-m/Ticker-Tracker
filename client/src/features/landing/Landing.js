97960a06ee6f28c346ed1d8ef283c34226498f61
k1te <kmiller343@gmail.com>
2021-03-29 11:53:57 -0500

    basic landing component

---
+++ client/src/features/landing/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <h3>Welcome to Ticker Tracker!</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
