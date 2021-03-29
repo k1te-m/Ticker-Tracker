a7fc6472b48b00c2a300c14fd5cab6db4dc6857e
k1te <kmiller343@gmail.com>
2021-03-29 14:03:51 -0500

    API routes for login/registration

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,3 +3,96 @@ const bcrypt = require("bcryptjs");
 const config = require("config");
 const jwt = require("jsonwebtoken");
 const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+
+let jwtENV;
+
+if (process.env.NODE_ENV !== "production") {
+  jwtENV = config.get("JWT_SECRET");
+} else {
+  jwtENV = process.env.JWT_SECRET;
+}
+
+// @route POST api/auth/register
+// @desc Register user
+// @access Public
+router.post("/register", async (req, res) => {
+  const { name, username, email, password } = req.body;
+
+  // Validation
+  if (!name || !username || !email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (user) throw Error("User already exists");
+
+    const salt = await bcrypt.genSalt(10);
+    if (!salt) throw Error("Something went wrong with bcrypt");
+
+    const hash = await bcrypt.hash(password, salt);
+    if (!hash) throw Error("Something went wrong with password hashing");
+
+    const newUser = new User({
+      name,
+      username,
+      email,
+      password: hash,
+    });
+
+    const savedUser = await newUser.save();
+    if (!savedUser) throw Error("Something went wrong saving the user");
+
+    const token = jwt.sign({ id: savedUser._id }, jwtENV, { expiresIn: "1h" });
+
+    res.status(200).json({
+      token,
+      user: {
+        _id: savedUser.id,
+        name: savedUser.name,
+        username: savedUser.username,
+        email: savedUser.email,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+});
+
+// @route POST api/auth/login
+// @desc Login user
+// @access Public
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  // Validation
+  if (!email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) throw Error("User does not exist");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw Error("Invalid credentials");
+
+    const token = jwt.sign({ id: user._id }, jwtENV, { expiresIn: "1h" });
+    if (!token) throw Error("Could not sign the token");
+
+    res.status(200).json({
+      token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
+});
+
+module.exports = router;
