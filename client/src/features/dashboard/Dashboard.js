3f66ed60b45d61b12a5fbaa063d2c2d354db0675
k1te <kmiller343@gmail.com>
2021-03-29 17:41:10 -0500

    imported search/sidebar

--- client/src/features/dashboard/Dashboard.js
+++ client/src/features/dashboard/Dashboard.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selectAuth, loadUser } from "../auth/authSlice";
+import Footer from "../footer/Footer";
+import Search from "./search/Search";
+import Sidebar from "./sidebar/Sidebar";
 
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  const dispatch = useDispatch();
+  const auth = useSelector(selectAuth);
+
+  console.log(auth);
+
+  useEffect(() => {
+    if (!auth.user) {
+      dispatch(loadUser());
+    }
+  }, [auth.user, dispatch]);
+
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-3 p-0">
+            <Sidebar />
+          </div>
+          <div className="col p-0">
+            <Search />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Dashboard;
