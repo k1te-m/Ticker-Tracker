75cc17f55768f8c7aefd9e90cfdf292f5b79743a
k1te <kmiller343@gmail.com>
2021-03-30 16:47:29 -0500

    updated searchTicker API function

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,14 +1,9 @@
 import axios from "axios";
-const baseURL = "https://cloud.iexapis.com/stable/stock/";
-const searchQuery = "/batch?types=quote,news,chart&range=1m&last=10&token=";
-
-const key = process.env.IEX_KEY;
 
 export default {
   searchTicker: (ticker) => {
     delete axios.defaults.headers.common["x-auth-token"];
-    console.log(key);
 
-    return axios.get(baseURL + ticker + searchQuery + key);
+    return axios.get("/api/ticker/query/" + ticker);
   },
 };
