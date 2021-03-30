0d0c97e86429d98bcb26ffdd7d9661e0c74a216d
k1te <kmiller343@gmail.com>
2021-03-30 14:36:48 -0500

    imported searchReducer

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import alertReducer from "../features/alert/alertSlice";
+import searchReducer from "../features/dashboard/search/searchSlice";
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     alert: alertReducer,
+    search: searchReducer,
   },
 });
