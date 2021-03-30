009050e02919201506e435c46f8d42102751c1cd
k1te <kmiller343@gmail.com>
2021-03-30 14:52:17 -0500

    updated key variable

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const baseURL = "https://cloud.iexapis.com/stable/stock/";
 const searchQuery = "/batch?types=quote,news,chart&range=1m&last=10&token=";
 
-let key = process.env.IEX_KEY;
+const key = process.env.IEX_KEY;
 
 export default {
   searchTicker: (ticker) => {
