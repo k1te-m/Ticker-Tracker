fce8e99a7744b90591f442a073edbb22ba0b8b59
k1te <kmiller343@gmail.com>
2021-03-30 16:47:53 -0500

    imported ticker routes to index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const authRoutes = require("./auth");
+const tickerRoutes = require("./ticker");
 
 router.use("/auth", authRoutes);
+router.use("/ticker", tickerRoutes);
 
 module.exports = router;
