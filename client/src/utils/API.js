d27d947dcf1a4baae1ef0f3ea73a76d13e09383f
k1te <kmiller343@gmail.com>
2021-03-31 15:09:25 -0500

    API funcitons for watch/retrieve symbols

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,4 +6,10 @@ export default {
 
     return axios.get("/api/ticker/query/" + ticker);
   },
+  watchStock: async (id, symbol) => {
+    return await axios.put("/api/ticker/follow/" + id, symbol);
+  },
+  getSymbols: async (id) => {
+    return await axios.get("/api/ticker/symbols/" + id);
+  },
 };
