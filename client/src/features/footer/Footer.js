404c8ecef6eab5039868718ca9548e2f5ba74470
k1te <kmiller343@gmail.com>
2021-04-06 14:44:28 -0500

    added link to github repo

--- client/src/features/footer/Footer.js
+++ client/src/features/footer/Footer.js
@@ -2,13 +2,24 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <div className="footer p-2">
       <div className="container-fluid">
         <div className="row">
-          <a href="https://iexcloud.io">Data provided by IEX Cloud</a>
+          <a href="https://iexcloud.io" target="_blank" rel="noreferrer">
+            Data provided by IEX Cloud
+          </a>
         </div>
         <div className="row">
           <span>Ticker Tracker © 2021</span>
+        </div>
+        <div className="row">
+          <a
+            href="https://github.com/k1te-m/Ticker-Tracker"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-github" />
+          </a>
         </div>
       </div>
     </div>
