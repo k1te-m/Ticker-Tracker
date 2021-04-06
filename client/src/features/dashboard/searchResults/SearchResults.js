23eb9a4d954df1cb3303da01ed3616310ea55729
k1te <kmiller343@gmail.com>
2021-04-06 16:29:30 -0500

    conditional render for gain/loss

--- client/src/features/dashboard/searchResults/SearchResults.js
+++ client/src/features/dashboard/searchResults/SearchResults.js
@@ -40,42 +40,88 @@ const SearchResults = () => {
       </div>
     ));
 
-    tickerCard = (
-      <div className="container stock-info my-auto">
-        <div className="row">
-          <div className="col-12">
-            <h6>
-              {companyName}, ({symbol})
-            </h6>
+    if (change >= 0) {
+      tickerCard = (
+        <div className="container stock-info my-auto">
+          <div className="row">
+            <div className="col-12">
+              <h6>
+                {companyName}, ({symbol})
+              </h6>
+            </div>
+          </div>
+          <div className="row mb-2">
+            <WatchButton />
+          </div>
+          <div className="row">
+            <h5 className="latest-price gain">
+              ${latestPrice} <span className="time-stamp">({latestTime})</span>
+            </h5>
+          </div>
+          <div className="row mb-2">
+            <div className="col-12 prev-close">
+              Previous Close: ${previousClose}
+            </div>
+          </div>
+          <div className="row mb-1">
+            <div className="col-6 dollar-change gain">
+              <i className="fas fa-long-arrow-alt-up" /> ${change} Today
+            </div>
+            <div className="col-6 percent-change gain">
+              <i className="fas fa-long-arrow-alt-up" />
+              {changePercent}%
+            </div>
+          </div>
+          <div className="row week pb-2">
+            <div className="col">
+              <span className="high">52 Week High: ${week52High}</span>
+            </div>
+            <div className="col">
+              <span className="low">52 Week Low: ${week52Low}</span>
+            </div>
           </div>
         </div>
-        <div className="row mb-2">
-          <WatchButton />
-        </div>
-        <div className="row">
-          <h5 className="latest-price">
-            ${latestPrice} <span className="time-stamp">({latestTime})</span>
-          </h5>
-        </div>
-        <div className="row mb-1">
-          <div className="col-6 dollar-change">${change} Today</div>
-          <div className="col-6 percent-change">{changePercent}%</div>
-        </div>
-        <div className="row mb-2">
-          <div className="col-12 prev-close">
-            Previous Close: ${previousClose}
+      );
+    } else if (change < 0) {
+      tickerCard = (
+        <div className="container stock-info my-auto">
+          <div className="row">
+            <div className="col-12">
+              <h6>
+                {companyName}, ({symbol})
+              </h6>
+            </div>
+          </div>
+          <div className="row mb-2">
+            <WatchButton />
+          </div>
+          <div className="row">
+            <h5 className="latest-price loss">
+              ${latestPrice} <span className="time-stamp">({latestTime})</span>
+            </h5>
+          </div>
+          <div className="row mb-2">
+            <div className="col-12 prev-close">
+              Previous Close: ${previousClose}
+            </div>
+          </div>
+          <div className="row mb-1">
+            <div className="col-6 dollar-change loss">
+              <i className="fas fa-long-arrow-alt-down" />${change} Today
+            </div>
+            <div className="col-6 percent-change loss">{changePercent}%</div>
+          </div>
+          <div className="row week pb-2">
+            <div className="col">
+              <span className="high">52 Week High: ${week52High}</span>
+            </div>
+            <div className="col">
+              <span className="low">52 Week Low: ${week52Low}</span>
+            </div>
           </div>
         </div>
-        <div className="row week pb-2">
-          <div className="col">
-            <span className="high">52 Week High: ${week52High}</span>
-          </div>
-          <div className="col">
-            <span className="low">52 Week Low: ${week52Low}</span>
-          </div>
-        </div>
-      </div>
-    );
+      );
+    }
   }
 
   return (
