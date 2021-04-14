53748044b4f8ceb88c4e23059a0b353e5d8ebb7b
k1te <kmiller343@gmail.com>
2021-04-14 14:08:32 -0500

    loading component

---
+++ client/src/features/loading/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="load col-12 justify-content-center">
+      <i class="fas fa-spinner fa-spin" />
+    </div>
+  );
+};
+
+export default Loading;
