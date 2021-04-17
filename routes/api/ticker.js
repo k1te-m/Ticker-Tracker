e3118f1542afd696696467e2f6bc29a61f46ceb1
k1te <kmiller343@gmail.com>
2021-04-17 11:49:12 -0500

    updated error response

--- routes/api/ticker.js
+++ routes/api/ticker.js
@@ -43,7 +43,7 @@ router.get("/batchquery/:tickers", async (req, res) => {
 
     res.send(response.data);
   } catch (error) {
-    restart.status(500).json({
+    res.status(500).json({
       success: false,
       message: error.message,
     });
